b5f6acf41215533699fd78fb14f947b0884d0050
zsp <15396226187@163.com>
2021-04-23 22:21:12 +0800

    查找半天发现并没有开放沙箱，微信支付api对接失败

--- wxpay-study/src/main/java/com/zsp/WxpayStudyApplication.java
+++ wxpay-study/src/main/java/com/zsp/WxpayStudyApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//微信不支持个人用户使用，直接裂开
 public class WxpayStudyApplication {
 
     public static void main(String[] args) {
