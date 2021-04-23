020c17083cb3402aab40e2561ba79817ff2eb454
zsp <15396226187@163.com>
2021-04-23 22:08:12 +0800

    正在申请微信商家，准备对接微信支付api

---
+++ wxpay-study/src/main/java/com/zsp/WxpayStudyApplication.java
@@ -0,0 +1,13 @@
+package com.zsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxpayStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxpayStudyApplication.class, args);
+    }
+
+}
---
+++ wxpay-study/src/test/java/com/zsp/WxpayStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxpayStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
